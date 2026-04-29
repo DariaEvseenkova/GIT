27ab73b0475c7ecbcd840640da28d6ea375ec051
Daria Evseenkova <dariaevseenkova@gmail.com>
2026-04-29 16:41:03 +0300

    Вторая новая фича
    Вот такая вот фича

--- age.py
+++ age.py
@@ -1,7 +1,12 @@
 age = int(input())
-if age == 18:
-    print("Доступ запрещен")
+if age >= 50:
+    health = input("Как вы себя чувствуете?")
+    print(health)
+    if health == "хорошо":
+        print("Поздравляю!")
 elif 7 < age < 18:
     print("Иди делай домашку")
+elif age > 18:
+    print("Доступ запрещен")
 else:
     print("Доступ разрешен")
